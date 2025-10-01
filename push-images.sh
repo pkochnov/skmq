7eff7fd06c746a32c0c47ad8bb1a34779883feca
Andrey <andrey@kochnov.ru>
2025-10-01 20:30:16 +0300

    postgres

--- push-images.sh
+++ push-images.sh
@@ -1,3 +1,5 @@
 DIR=${dirname $0}
 
 discli --insecure push ${DIR}/images/arangodb:3.11.12 registry:5000/arangodb:3.11.12
+
+discli --insecure push ${DIR}/images/postgres:16.6 registry:5000/postgres:16.6
