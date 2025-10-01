f954920cc774a4238e2d0842897404c4150bb603
Andrey <andrey@kochnov.ru>
2025-10-01 20:39:07 +0300

    redis

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,7 @@
 DIR=${dirname $0}
 
-discli --insecure push ${DIR}/images/arangodb:3.11.12 registry:5000/arangodb:3.11.12
+discli --insecure push ${DIR}/images/arangodb-3.11.12.tar registry:5000/arangodb:3.11.12
 
-discli --insecure push ${DIR}/images/postgres:16.6 registry:5000/postgres:16.6
+discli --insecure push ${DIR}/images/postgres-16.6.tar registry:5000/postgres:16.6
+
+discli --insecure push ${DIR}/images/redis-7.4.2.tar registry:5000/redis:7.4.2
