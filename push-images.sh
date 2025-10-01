abf09b4a4d8410de2326425c464e9450d0b77d0b
Andrey <andrey@kochnov.ru>
2025-10-01 20:46:01 +0300

    victoria metrics

--- push-images.sh
+++ push-images.sh
@@ -5,3 +5,5 @@ discli --insecure push ${DIR}/images/arangodb-3.11.12.tar registry:5000/arangodb
 discli --insecure push ${DIR}/images/postgres-16.6.tar registry:5000/postgres:16.6
 
 discli --insecure push ${DIR}/images/redis-7.4.2.tar registry:5000/redis:7.4.2
+
+discli --insecure push ${DIR}/images/victoria-metrics-v1.108.1.tar registry:5000/victoriametrics/victoria-metrics:v1.108.1
